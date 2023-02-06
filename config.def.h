453b3207a079b71fc82d659ded4167f5584b4986
EscherMoore <EscherMoore@users.noreply.github.com>
2023-02-06 10:14:59 -0500

    tweak the colors of the gruvbox color scheme

--- config.def.h
+++ config.def.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#98971a" },
-	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
+	[SchemeNorm] = { "#928374", "#282828" },
+	[SchemeSel] = { "#282828", "#d79921" },
+	[SchemeOut] = { "#282828", "#fabd2f" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 5;
